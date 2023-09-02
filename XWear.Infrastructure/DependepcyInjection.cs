fdd1ec9d8927cbbb579b23dd4d1cf0190c9ed4b9
paterfamelias@gmail.com <paterfamelias@gmail.com>
2023-09-02 06:32:02 +0600

    Added

--- XWear.Infrastructure/DependepcyInjection.cs
+++ XWear.Infrastructure/DependepcyInjection.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 using XWear.Application.Common.Interfaces;
 using XWear.Infrastructure.Authentication;
 using XWear.Infrastructure.Authentication.Settings;
@@ -10,13 +14,39 @@ namespace XWear.Infrastructure
 {
     public static class DependepcyInjection
     {
-        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        public static IServiceCollection AddInfrastructure(
+            this IServiceCollection services, 
+            IConfiguration configuration)
         {
-            services.Configure<JwtSettings>(configuration.GetSection(nameof(JwtSettings)));
-
-            services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
+            services.AddAuth(configuration);
             services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
             services.AddScoped<IUserRepository, UserRepository>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddAuth(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            var jwtSettings = new JwtSettings();
+            configuration.Bind(nameof(JwtSettings), jwtSettings);
+            services.AddSingleton(Options.Create(jwtSettings));
+
+            services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
+            services
+                .AddAuthentication(defaultScheme: JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options => options.TokenValidationParameters = new TokenValidationParameters
+                {
+                   ValidateIssuer = true,
+                   ValidateAudience = true,
+                   ValidateLifetime = true,
+                   ValidateIssuerSigningKey = true,
+                   ValidIssuer = jwtSettings.Issuer,
+                   ValidAudience = jwtSettings.Audience,
+                   IssuerSigningKey = new SymmetricSecurityKey(
+                       Encoding.UTF8.GetBytes(jwtSettings.Secret))
+                });
 
             return services;
         }
