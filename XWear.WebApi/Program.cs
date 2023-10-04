c6fc2ac194eeaf40f294d5c69e9c1a5591c393ba
paterfamelias@gmail.com <paterfamelias@gmail.com>
2023-10-05 01:43:03 +0600

    Added

--- XWear.WebApi/Program.cs
+++ XWear.WebApi/Program.cs
@@ -1,20 +1,16 @@
-using Microsoft.AspNetCore.Mvc.Infrastructure;
-using System.Reflection;
-using XWear.Application;
-using XWear.Domain.Common.Enums;
-using XWear.Domain.Common.Extensions;
-using XWear.Infrastructure;
-using XWear.WebApi.Common.Constants;
-using XWear.WebApi.Common.Errors;
 using XWear.WebApi.Common.Extensions;
-using XWear.WebApi.Configurations.CorsConfig;
-using XWear.WebApi.Configurations.Localization;
-using XWear.WebApi.Configurations.Swagger;
 
 namespace XWear.WebApi;
 
+/// <summary>
+/// Главный класс приложения.
+/// </summary>
 public class Program
 {
+    /// <summary>
+    /// Главный метод приложения.
+    /// </summary>
+    /// <param name="args">Массив аргументов командной строки.</param>
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
