c9939a551bf40cc3a14e4a7fde705730fe569516
paterfamelias@gmail.com <paterfamelias@gmail.com>
2023-10-05 01:27:59 +0600

    Added

--- XWear.WebApi/Program.cs
+++ XWear.WebApi/Program.cs
@@ -18,8 +18,11 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
-        var app = builder.Build();
+
         builder.Services.AddApiServices(builder.Configuration);
+
+        var app = builder.Build();
+
         app.AddApiSettings();
         app.Run();
     }
