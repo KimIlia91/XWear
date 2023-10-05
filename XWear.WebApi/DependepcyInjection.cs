5d37d217ccce8ae20237934bdf79a5864fe756b1
paterfamelias@gmail.com <paterfamelias@gmail.com>
2023-10-05 11:51:30 +0600

    added

--- XWear.WebApi/DependepcyInjection.cs
+++ XWear.WebApi/DependepcyInjection.cs
@@ -1,31 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc.Infrastructure;
 using System.Reflection;
-using XWear.Application;
 using XWear.Domain.Common.Enums;
 using XWear.Domain.Common.Extensions;
-using XWear.Infrastructure;
 using XWear.WebApi.Common.Configurations.CorsConfig;
 using XWear.WebApi.Common.Configurations.Localization;
 using XWear.WebApi.Common.Configurations.Swagger;
 using XWear.WebApi.Common.Constants;
 using XWear.WebApi.Common.Errors;
 
-namespace XWear.WebApi.Common.Extensions
+namespace XWear.WebApi
 {
-    /// <summary>
-    /// Расширение для добавления кастомных служб в контейнер служб ASP.NET Core.
-    /// </summary>
-    public static class ServiceCollectionExtensions
+    public static class DependepcyInjection
     {
-        /// <summary>
-        /// Добавляет кастомные службы в контейнер служб ASP.NET Core.
-        /// </summary>
-        /// <param name="services">Контейнер служб (IServiceCollection).</param>
-        /// <param name="configuration">Конфигурация приложения.</param>
-        /// <returns>Контейнер служб с добавленными кастомными службами.</returns>
         public static IServiceCollection AddPresentation(
-            this IServiceCollection services,
-            IConfiguration configuration)
+           this IServiceCollection services,
+           IConfiguration configuration)
         {
             services.AddHttpContextAccessor();
             services.AddAuthorization();
